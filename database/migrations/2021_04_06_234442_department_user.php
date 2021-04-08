128eec37db6372156cd5f9b66cff3b51588d908b
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-04-09 00:09:18 +0800

    Add Department Relationship

---
+++ database/migrations/2021_04_06_234442_department_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DepartmentUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('department_user', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('department_id');
+            $table->uuid('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('department_user');
+    }
+}
